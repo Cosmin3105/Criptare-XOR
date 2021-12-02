5841eadbdbd2f6d497f4a32e67842900af9ed61c
Cosmin Colceru <92436405+Cosmin3105@users.noreply.github.com>
2021-12-02 19:46:11 +0200

    update

--- encrypt.py
+++ encrypt.py
@@ -7,9 +7,19 @@ output_file = sys.argv[3]
 with open(input_file, 'r') as f:
     text = f.read()
 
+ok = 1
 p_index = 0
 with open(output_file, 'wb') as f:
     for c in text:
-        encrypted_c = ord(c) ^ ord(parola[p_index])
+        if ord(c) < 128:
+            encrypted_c = ord(c) ^ ord(parola[p_index])
+        else:
+            encrypted_c = ord('?') ^ ord(parola[p_index])
+            ok = 0
+
         p_index = (p_index + 1) % len(parola)
         f.write(encrypted_c.to_bytes(1, byteorder='big'))
+
+if not ok:
+    print("Unele caractere nu au putut fi criptate")
+
